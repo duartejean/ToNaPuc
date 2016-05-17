28c9dae8678a353e66ded9bb073035d29ca13a83
Jean Duarte <jeaan.duarte@hotmail.com>
2016-05-16 22:13:59 -0300

    Class created with the implementation of access to GPS and user location. Creation of the fields on the screen to display the information. Function to calculate the distance between the points.

--- app/src/main/java/br/com/tonapuc/MainActivity.java
+++ app/src/main/java/br/com/tonapuc/MainActivity.java
@@ -1,13 +1,50 @@
 package br.com.tonapuc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private TextView lblLatitude, lblLongitude, lblDistancia;
+    private static final double latitudePUC = -19.955055;
+    private static final double longitudePUC = -44.198153;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lblLatitude = (TextView) findViewById(R.id.lbl_latitude);
+        lblLongitude = (TextView) findViewById(R.id.lbl_longitude);
+        lblDistancia = (TextView) findViewById(R.id.lbl_distancia);
+    }
+
+    public void Localizar(View view) {
+        GPSTracker Local = new GPSTracker(this);
+        lblLatitude.setText(String.format("%s", Local.getLatitude()));
+        lblLongitude.setText(String.format("%s", Local.getLongitude()));
+
+        double distancia = DistanciaEntrePontos();
+        if (distancia > 500) {
+            lblDistancia.setText(String.format("Você está longe da PUC Minas em Betim, a %sm de distância.", distancia));
+        } else {
+            lblDistancia.setText(String.format("Você está dentro do ambiente da PUC Minas em Betim!"));
+        }
+    }
+
+    private double DistanciaEntrePontos() {
+        GPSTracker Local = new GPSTracker(this);
+
+        Location locationA = new Location("PUC Minas");
+        locationA.setLatitude(latitudePUC);
+        locationA.setLongitude(longitudePUC);
+
+        Location locationB = new Location("Smartphone");
+        locationB.setLatitude(Local.getLatitude());
+        locationB.setLongitude(Local.getLongitude());
+
+        return locationA.distanceTo(locationB);
     }
 }
